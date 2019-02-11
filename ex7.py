bca668e27fc7739479c787a6fde26308f5d11287
Deepak Mahakale <deepakmahakale@gmail.com>
2019-02-12 01:29:37 +0530

    Exercise 7 - Strings

---
+++ ex7.py
@@ -0,0 +1,22 @@
+print('Mary had a little lamb.')
+print('Its fleece was white as {}.'.format('snow'))
+print('And everywhere that Mary went.')
+print('.' * 10) # This will print . ten times
+
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+
+# The value of end will be replace the default (newline) after print statement.
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ') 
+print(end7 + end8 + end9 + end10 + end11 + end12)
