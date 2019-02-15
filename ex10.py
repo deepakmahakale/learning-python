2a28b4cf255fae1207564a4ed6e64ceb58d8c209
Deepak Mahakale <deepakmahakale@gmail.com>
2019-02-15 22:48:21 +0530

    Exercise 10 - Strings

---
+++ ex10.py
@@ -0,0 +1,10 @@
+print("""
+    First long string
+    Second string
+    3rd string
+    Fourth string
+""")
+
+while True:
+    for i in ["/","- ","|","\\","|"]:
+         print("%s\r" % i, end ='')
